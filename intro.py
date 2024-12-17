951e10f51c8299064dc283875f81344150305169
Tannaz Y <tannaz.yadollahi@elev.ga.ntig.se>
2024-12-17 10:38:19 +0100

    övningar

--- intro.py
+++ intro.py
@@ -1,1 +1,70 @@
-print("Hello woroorld!")
+'''
+name = "Bengt"
+age1 = 30
+age2= 40
+
+print(f"Tja {name}, är din ålder {age1}?, du ser ut o vara {age2}...")
+'''
+
+
+
+
+'''
+#If och Elif sats
+x = 10
+b = 15
+if x > b:
+    print("x är mindre än b")
+elif b > x:
+    print ("b är större än x ")
+
+
+#if elif else sats
+x = 10
+b = 15
+if x > b:
+    print("x är mindre än b")
+elif b == x:
+    print ("b och x är lika mycket ")
+else: 
+    print ("b är större än x")
+'''
+
+'''
+#Beräkna summan tillsammans 
+x = 10 
+y = 20 
+
+print(f"summan av {x}+{y} är",(x+y))
+'''
+
+'''
+#While loop
+i = 1
+while i < 6:
+    print (i)
+    i +=1
+
+#While loop break
+i = 1 
+while i < 6:
+    print (i)
+    if i == 3:
+     break
+    i +=1
+'''
+
+
+
+
+#Heltal konvertering
+x=42
+y = (x)
+print(y)
+print(type(y))
+
+#Flyttal konvertering
+x=3.60
+y= (x)
+print(y)
+print(type(y))
