2ed46037bf7644c19de528ac81b2cb4c2c6830a6
Tannaz Y <tannaz.yadollahi@elev.ga.ntig.se>
2024-12-18 14:02:13 +0100

    done

--- listor.py
+++ listor.py
@@ -37,3 +37,83 @@ thislist = list(("apple", "banana", "cherry")) #Dubbel paranteser
 print(thislist)
 '''
 
+
+
+
+
+'''
+#printas enskilt vertikalt
+my_list = ["apple", "banana", "cherry"]
+for fruit in my_list:
+
+    print(fruit)
+'''
+
+
+
+
+'''
+#for, if, else
+my_list = ["apple", "banana", "cherry"]
+for fruit in my_list:
+    if fruit !="cherry": #else satsen kommer in här 
+
+        print("I like " + fruit)
+    else: print("I don't like " + fruit)
+'''
+
+
+
+'''
+#Lägga till objekt i lista
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Radera objekt från lista
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("apple")
+print(thislist)
+'''
+
+'''
+#Apple start på 0, banana 1 cherry är 2 därför byts den ut till pear
+my_list = ["apple", "banana", "cherry"]
+my_list[2] = "pear"
+print(my_list)
+
+my_list.insert(3, "orange")
+print(my_list)
+
+my_list.append("grape")
+print(my_list)
+'''
+
+
+
+
+'''
+lista = ["Stockholm", "Göteborg", "Nynäshamn", "Nyköping"]
+lista.append("Linköping")
+print(lista)
+
+lista.remove("Göteborg")
+print(lista)
+'''
+
+
+
+
+'''
+lista = ["Stockholm", "Göteborg", "Nynäshamn", "Nyköping"]
+for x in lista:
+  print (x)
+  if x =="Nyköping":
+   break
+
+lista2 = [4, 8, 10, 30, 20]
+summera = sum(lista2)
+
+print(f"Summan är:  {summera}")
+'''
+
