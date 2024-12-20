278216283f7fc5b7d3675ef989b52ccd52fd6c73
Tannaz Y <tannaz.yadollahi@elev.ga.ntig.se>
2024-12-20 10:19:00 +0100

    lol

---
+++ Dictionary.py
@@ -0,0 +1,23 @@
+#Dictionary
+'''
+persons = { "name": "Bengt","age": 45, "city": "Stockholm"}
+
+for attribut , värde in persons.items():
+    print(f'persones{attribut}): är  {värde}')
+'''
+
+
+
+# Lista
+'''
+fruits = ["äpple", "banan", "körsbär"]
+'''
+# Tuppel
+'''
+coordinates = (10, 20, 30)
+'''
+# Dictionary
+'''
+person = {"namn": "Anna", "ålder": 25}
+'''
+
---
+++ if, else elif osv.py
@@ -0,0 +1,37 @@
+#If och Elif sats
+'''
+x = 10
+b = 15
+if x > b:
+    print("x är mindre än b")
+elif b > x:
+    print ("b är större än x ")
+'''
+
+#if elif else sats
+'''
+x = 10
+b = 15
+if x > b:
+    print("x är mindre än b")
+elif b == x:
+    print ("b och x är lika mycket ")
+else: 
+    print ("b är större än x")
+'''
+
+#While loop
+'''
+i = 1
+while i < 6:
+    print (i)
+    i +=1
+
+#While loop break
+i = 1 
+while i < 6:
+    print (i)
+    if i == 3:
+     break
+    i +=1
+'''
